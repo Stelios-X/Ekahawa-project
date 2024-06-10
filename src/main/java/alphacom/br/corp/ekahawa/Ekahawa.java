17c7fa8d8c821ea43d29b396d39fa04523d4339f
brandon <brandon@toysoldier>
2024-06-10 06:51:36 +0300

    Best time to buy and sell stocks solution in Java.

--- src/main/java/alphacom/br/corp/ekahawa/Ekahawa.java
+++ src/main/java/alphacom/br/corp/ekahawa/Ekahawa.java
@@ -4,8 +4,6 @@
 
 package alphacom.br.corp.ekahawa;
 
-import java.util.Arrays;
-
 /**
  *
  * @author brandon
@@ -16,7 +14,7 @@ public class Ekahawa {
         System.out.println("Hello World!");
         
         int[] nums = {7,1,5,3,6,4};
-        int target = 6;
+        //int target = 6;
         
         //Vuyanzi vuyanzi = new Vuyanzi();
         //System.out.println(Arrays.toString(vuyanzi.twoSumSolution(nums, target)));
